cb83895904e13f16105ff970f691e5d1f96d45f1
Vinay Motapalukula <vinaychinna0804@gmail.com>
2025-04-16 18:48:46 +0530

    Added backend code setup

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ path : "./.env" });
+
+
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on PORT : ${port}`);
+})
